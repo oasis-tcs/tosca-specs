6f6c535760706d5e16e3bd142ea5d6faf534fb17
Paul Jordan <paul.m.jordan@outlook.com>
2024-11-18 19:00:19 +0000

    Add python test

--- tosca_2_0/examples/README.md
+++ tosca_2_0/examples/README.md
@@ -3,4 +3,7 @@ This directory contains the examples used in the TOSCA v2.0
 specification. These examples are validated here before they are
 included in the spec.
 
-A count of valid and invalid examples and details of the first invalid example. can be obtained with the bash script bash_test.sh
+The validation tests can be run using either
+1) bash_test.sh gives a count of valid and invalid examples and details of the first invalid example
+or
+2) python unittest test_examples.py gives pass or fail with reason for all the examples.
--- tosca_2_0/test_examples.py
+++ tosca_2_0/test_examples.py
@@ -2,22 +2,25 @@ import os
 import subprocess
 import unittest
 
-class TestYAMLFiles(unittest.TestCase):
+def generate_test_classes():
+    directory = 'tosca_2_0/examples'
+    test_classes = {}
 
-    def test_yaml_files(self):
-        # # Directory to search for .yaml files
-        directory = 'tosca_2_0/examples'
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.yaml'):
+                class_name = f"Test_{file.replace('.', '_')}"
+                test_classes[class_name] = type(class_name, (unittest.TestCase,), {
+                    'test_yaml_file': lambda self, file=file, root=root: self.assertEqual(
+                        subprocess.run(['puccini-tosca', 'parse', os.path.join(root, file)], capture_output=True).returncode,
+                        0,
+                        f"puccini failed for {file}"
+                    )
+                })
 
-        # # Find all .yaml files in the directory
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                if file.endswith('.yaml'):
-                    # Run puccini command on each .yaml file and check the result is zero
-                    print(f"Running puccini on {file} at {root}")
-                    print
-                    result = subprocess.run(['puccini-tosca', 'parse', os.path.join(root,file) ], capture_output=True)
-                    print(result.stderr,"\n")
-                    self.assertEqual(result.returncode, 0, f"puccini failed for {file}")
+    return test_classes
+
+globals().update(generate_test_classes())
 
 if __name__ == '__main__':
     unittest.main()
