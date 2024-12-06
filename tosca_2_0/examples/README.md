23a79eed91563eab0c186aade9abdec4b27c759d
Paul Jordan <paul.m.jordan@outlook.com>
2024-12-06 18:14:19 +0000

    first batch of examples (#217)
    This branch was forked from 2225ab8b and so has many conflicts. I
    suggest the easiest way to deal with this is to back out the most recent
    commit in working which is 215302cd before applying this PR. There
    should then be no conflicts.

--- tosca_2_0/examples/README.md
+++
@@ -1,4 +0,0 @@
-# TOSCA Examples
-This directory contains the examples used in the TOSCA v2.0
-specification. These examples are validated here before they are
-included in the spec.
---
+++ tosca_2_1/test_examples.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+import subprocess
+
+def generate_test_classes():
+    directory = 'tosca_2_1/examples'
+    test_classes = {}
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.yaml'):
+                class_name = f"Test_{file.replace('.', '_')}"
+                def test_method(self, file=file, root=root):
+                    result = subprocess.run(['puccini-tosca', 'parse', os.path.join(root, file)], capture_output=True)
+                    self.assertEqual(result.returncode, 0, f"puccini failed for {file}\n{result.stderr.decode()}")
+                test_classes[class_name] = type(class_name, (unittest.TestCase,), {
+                    'test_yaml_file': test_method
+                })
+
+    return test_classes
+
+# Generate test classes and add them to the current module
+test_classes = generate_test_classes()
+globals().update(test_classes)
+
+if __name__ == '__main__':
+    unittest.main()
